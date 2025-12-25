3c50b76673337725b4f54d52561bd6aa9ba71116
Dylan Manseri <manseri.dylan1@gmail.com>
2025-12-25 18:03:01 +0100

    ajout du code du site

---
+++ CodeSite/secret/config.conf.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'hostname' => 'postgresql-superairport3000.alwaysdata.net',
+    'username' => 'superairport3000',
+    'password' => 'mdrpsecretdelastreetzer',
+    'dbname' => 'superairport3000_bd',
+    'port' => 5432
+];
