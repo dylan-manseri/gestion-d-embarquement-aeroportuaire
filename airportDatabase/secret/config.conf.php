e12c2d0b8a482938bfaec3b4d4ffd6a43078ffd6
Dylan Manseri <manseri.dylan1@gmail.com>
2025-12-25 18:02:21 +0100

    Delete airportDatabase directory

--- airportDatabase/secret/config.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'hostname' => 'postgresql-superairport3000.alwaysdata.net',
-    'username' => 'superairport3000',
-    'password' => 'mdrpsecretdelastreetzer',
-    'dbname' => 'superairport3000_bd',
-    'port' => 5432
-];
